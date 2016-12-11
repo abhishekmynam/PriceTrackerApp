76b94dc81e029328102d1d3ef380114368aee5d7
abhishekmynam <abhishek.mynam@gmail.com>
2016-12-11 11:32:02 -0500

    added modules
    added eslint

--- gulpfile.js
+++ gulpfile.js
@@ -11,8 +11,8 @@ var open = require('gulp-open'); //open url in a web browser
 var browserify = require('browserify');//bundle js
 var reactify = require('reactify');// converts react js to normal js
 var source = require('vinyl-source-stream');// use conventional text stream with gulp
-//var concat = require('gulp-concat');//concatenate files
-//var lint = require('gulp-eslint');//lint js and jsx files, maintain quality of code we are writing
+var concat = require('gulp-concat');//concatenate files
+var lint = require('gulp-eslint');//lint js and jsx files, maintain quality of code we are writing
 
 
 var config={
@@ -62,14 +62,14 @@ gulp.task('js',function(){
         .pipe(gulp.dest(config.paths.dist + '/scripts'))
         .pipe(connect.reload());
 });
-/*
+
 
 gulp.task('css',function(){
     gulp.src(config.paths.css)
         .pipe(concat('bundle.css'))
         .pipe(gulp.dest(config.paths.dist+'/css'));
 });
-
+/*
 gulp.task('images',function(){
     gulp.src(config.paths.images)
         .pipe(gulp.dest(config.paths.dist+'/images'))
@@ -78,13 +78,13 @@ gulp.task('images',function(){
     gulp.src('./src/favicon.ico')
         .pipe(gulp.dest(config.paths.dist));
 });
-
+*/
 gulp.task('lint',function(){
     return gulp.src(config.paths.js)
         .pipe(lint({configFile: 'eslint.config.json'}))
         .pipe(lint.format());
 });
-*/
+
 gulp.task('watch',function(){
     gulp.watch(config.paths.html,['html']);
     gulp.watch(config.paths.js,['js','lint']);
@@ -92,4 +92,4 @@ gulp.task('watch',function(){
 
 //gulp.task('default',['html', 'js', 'images','css','lint','open','watch']); //list of tasks to run just by starting gulp without any parameters ir task names
 
-gulp.task('default',['html','open','watch', 'js']);
+gulp.task('default',['html','open','watch', 'js', 'css']);
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 /**
  * Created by abhis on 12/11/2016.
  */
+$ = jQuery = require('jquery');
 
 var App = console.log('hello from browserify');
 
